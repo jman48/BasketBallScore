02a61a88c5ae610ffc2a0082f9c8d94fffa2baf4
John Armstrong <j.armstrong484@gmail.com>
2015-11-11 10:14:41 +1300

    Base project created using yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bballApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bballApp
+ */
+angular.module('bballApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bballApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bballApp
+ */
+angular.module('bballApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
