1adb47f0cb1cb585c6fea12cb27278527007677a
WillHS <whardwicksmith@gmail.com>
2015-11-13 11:32:38 +1300

    Update user service tests to work with promises

--- test/spec/services/user.js
+++ test/spec/services/user.js
@@ -6,46 +6,71 @@ describe('Service: user', function () {
   beforeEach(module('bballApp'));
 
   // instantiate service
-  var user;
-  beforeEach(inject(function (_user_) {
+  var user, scope;
+  beforeEach(inject(function (_user_, _$rootScope_) {
     user = _user_;
+    scope = _$rootScope_;
   }));
 
   it('should do something', function () {
     expect(!!user).toBe(true);
   });
 
-  it('should return true when adding a username', function() {
-    var result = user.attemptRegister('John');
-    expect(result).toBe(true);
+  it('should resolve promise when adding a username', function() {
+    var promise = user.attemptRegister('John');
+    var spy = jasmine.createSpy('add user spy');
+    promise.then(spy);
+    scope.$apply();
+
+    expect(spy).toHaveBeenCalledWith('John');
   });
 
-  it('should return false when adding username twice', function() {
+  it('should reject promise when adding username twice', function() {
     user.attemptRegister('John');
-    var result = user.attemptRegister('John');
-    expect(result).toBe(false);
+    var promise = user.attemptRegister('John');
+    var spy = jasmine.createSpy('duplicate user spy');
+    promise.then(angular.noop, spy);
+    scope.$apply();
+
+    expect(spy).toHaveBeenCalled();
   });
 
-  it('should return true when adding two different usernames', function() {
+  it('should resolve promise when adding two different usernames', function() {
     user.attemptRegister('John');
-    var result = user.attemptRegister('Alice');
-    expect(result).toBe(true);
+    var promise = user.attemptRegister('Mary');
+    var spy = jasmine.createSpy('different users spy');
+    promise.then(spy);
+    scope.$apply();
+
+    expect(spy).toHaveBeenCalledWith('Mary');
   });
 
-  it('should return false when adding two different usernames with the first username repeated afterwards', function() {
+  it('should reject promise when adding two different usernames with the first username repeated afterwards', function() {
     user.attemptRegister('John');
     user.attemptRegister('Alice');
-    var result = user.attemptRegister('John');
-    expect(result).toBe(true);
+    var promise = user.attemptRegister('John');
+    var spy = jasmine.createSpy('different users spy');
+    promise.then(angular.noop, spy);
+    scope.$apply();
+
+    expect(spy).toHaveBeenCalled();
   });
 
-  it('should return false when username field is blank', function() {
-    var result = user.attemptRegister();
-    expect(result).toBe(false);
+  it('should reject promise when username field is blank', function() {
+    var promise = user.attemptRegister();
+    var spy = jasmine.createSpy('blank username');
+    promise.then(angular.noop, spy);
+    scope.$apply();
+
+    expect(spy).toHaveBeenCalled();
   });
 
-  it('should return false when username field is empty string', function() {
-    var result = user.attemptRegister();
-    expect(result).toBe(false);
+  it('should reject promise when username field is empty string', function() {
+    var promise = user.attemptRegister('');
+    var spy = jasmine.createSpy('empty string username');
+    promise.then(angular.noop, spy);
+    scope.$apply();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
