5b6e0b46d6258333c74d73efafccb008c87797c6
WillHS <whardwicksmith@gmail.com>
2015-11-16 13:11:40 +1300

    Basic login page working (no server comm).

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -9,19 +9,42 @@
  */
 angular.module('bballApp')
   .service('user', ['$q', function ($q) {
-    var users = ['reserved'];
+    var usernames = ['reserved'];
+
+    var users = {
+      'reserved': {
+        totalShots: 100,
+        highestStreak: 5,
+        shootOutsWon: 30
+      }
+    };
+
+    var currentUser = {};
+
     // AngularJS will instantiate a singleton by calling "new" on this function
     this.attemptRegister = function(username){
       var defer = $q.defer();
       if (!username)
         defer.reject("Username is not valid");
-      if (users.indexOf(username) == -1){
-        users.push(username);
+      if (usernames.indexOf(username) == -1){
+        usernames.push(username);
         defer.resolve(username);
       }
       else defer.reject("Username is already taken");
 
       return defer.promise;
-    }
+    };
+
+    this.attemptLogin = function(username){
+      var defer = $q.defer();
+      if (!username)
+        defer.reject("Username is not valid");
+      if (usernames.indexOf(username) !== -1){
+        currentUser = users[username];
+        defer.resolve(username);
+      }
+      else defer.reject("Username is not registered");
+      return defer.promise;
+    };
   }]);
 
