898f2be32b45588bf08a54428bda06e5f02a957d
Willhs <whardwicksmith@gmail.com>
2015-11-16 17:02:06 +1300

    Added simple loggin in tests for user service

--- test/spec/services/user.js
+++ test/spec/services/user.js
@@ -16,7 +16,7 @@ describe('Service: user', function () {
     expect(!!user).toBe(true);
   });
 
-  it('should resolve promise when adding a username', function() {
+  it('should succeed when registering a valid username', function() {
     var promise = user.attemptRegister('John');
     var spy = jasmine.createSpy('add user spy');
     promise.then(spy);
@@ -25,7 +25,7 @@ describe('Service: user', function () {
     expect(spy).toHaveBeenCalledWith('John');
   });
 
-  it('should reject promise when adding username twice', function() {
+  it('should fail when registering existing username', function() {
     user.attemptRegister('John');
     var promise = user.attemptRegister('John');
     var spy = jasmine.createSpy('duplicate user spy');
@@ -35,7 +35,7 @@ describe('Service: user', function () {
     expect(spy).toHaveBeenCalled();
   });
 
-  it('should resolve promise when adding two different usernames', function() {
+  it('should succeed when registering two different usernames', function() {
     user.attemptRegister('John');
     var promise = user.attemptRegister('Mary');
     var spy = jasmine.createSpy('different users spy');
@@ -45,7 +45,7 @@ describe('Service: user', function () {
     expect(spy).toHaveBeenCalledWith('Mary');
   });
 
-  it('should reject promise when adding two different usernames with the first username repeated afterwards', function() {
+  it('should fail when registering two different usernames with the first username repeated afterwards', function() {
     user.attemptRegister('John');
     user.attemptRegister('Alice');
     var promise = user.attemptRegister('John');
@@ -56,7 +56,7 @@ describe('Service: user', function () {
     expect(spy).toHaveBeenCalled();
   });
 
-  it('should reject promise when username field is blank', function() {
+  it('should fail when username field is blank', function() {
     var promise = user.attemptRegister();
     var spy = jasmine.createSpy('blank username');
     promise.then(angular.noop, spy);
@@ -65,7 +65,7 @@ describe('Service: user', function () {
     expect(spy).toHaveBeenCalled();
   });
 
-  it('should reject promise when username field is empty string', function() {
+  it('should fail when username field is empty string', function() {
     var promise = user.attemptRegister('');
     var spy = jasmine.createSpy('empty string username');
     promise.then(angular.noop, spy);
@@ -73,4 +73,39 @@ describe('Service: user', function () {
 
     expect(spy).toHaveBeenCalled();
   });
+
+  // LOGGING IN -----
+  it('should succeed when logging in as a valid user', function(){
+    var promise = user.attemptLogin('reserved');
+    var spy = jasmine.createSpy('valid user logon');
+    promise.then(spy);
+    scope.$apply();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should fail when logging in with an invalid username', function(){
+    var attempt1 = user.attemptLogin('');
+    var attempt2 = user.attemptLogin(null);
+
+    var spy = jasmine.createSpy('invalid username logon');
+
+    attempt1.then(angular.noop, spy);
+    scope.$apply();
+    expect(spy).toHaveBeenCalled();
+
+    attempt2.then(angular.noop, spy);
+    scope.$apply();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should fail when logging in as an unrecognised user', function(){
+    var promise = user.attemptLogin('Jimbo');
+    var spy = jasmine.createSpy('unrecognised user');
+    promise.then(angular.noop, spy);
+    scope.$apply();
+
+    expect(spy).toHaveBeenCalled();
+  });
+  // implement log out before more tests..
 });
