ef698e3b991cc4e7d990ccf1697681ba178d6881
thomsedavi <J8oit85y>
2015-11-17 15:06:44 +1300

    Added some tests to login controller

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -6,11 +6,17 @@ describe('Controller: LoginCtrl', function () {
   beforeEach(module('bballApp'));
 
   var LoginCtrl,
-    scope;
+    scope,
+    q,
+    user,
+    location;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function (_user_, $controller, $rootScope, $q, $location) {
     scope = $rootScope.$new();
+    q = $q;
+    location = $location;
+    user = _user_;
     LoginCtrl = $controller('LoginCtrl', {
       $scope: scope
       // place here mocked dependencies
@@ -27,4 +33,28 @@ describe('Controller: LoginCtrl', function () {
     }
     return defer.promise;
   }
+
+  it("should redirect when when given valid username", function(){
+    spyOn(location, 'path');
+    spyOn(user, "attemptLogin").and.returnValue(generatePromise(true, "John"));
+    var promise = scope.attemptLogin("John");
+    scope.$apply();
+    expect(location.path).toHaveBeenCalledWith('/landing');
+    expect(scope.errorMessage).toBe(undefined);
+  });
+
+  it("should stay on same page when given invalid username", function() {
+    spyOn(location, 'path');
+    spyOn(user, "attemptLogin").and.returnValue(generatePromise(false, "Error"));
+    var promise = scope.attemptLogin(null);
+    scope.$apply();
+    expect(location.path).not.toHaveBeenCalled();
+  });
+
+  it("should change error message if username is invalid", function() {
+    spyOn(user, "attemptLogin").and.returnValue(generatePromise(false, "Error"));
+    var promise = scope.attemptLogin(null);
+    scope.$apply();
+    expect(scope.errorMessage).toBe("Error");
+  });
 });
